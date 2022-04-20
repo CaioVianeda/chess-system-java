1db3248ff635e0cbb8c8c9f3c44137f89ba76189
CaioVianeda <caio.vianwda@gmail.com>
2022-04-20 13:45:46 -0300

    Possiveis movimentos de uma peça

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,7 +18,6 @@ public class Program {
 		while(true) {
 			
 			try {
-				UI.clearScreen();
 				UI.printBoard(chessMatch.getPieces());
 				System.out.println();
 				System.out.print("Origem: ");
